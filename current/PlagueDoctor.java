d7e05724e29776355d51fc5b2d26dedf6e5373c3
Soojin-C <schoi4@stuy.edu>
2017-11-13 16:31:09 -0500

    Finished PlagueDoctor class

---
+++ current/PlagueDoctor.java
@@ -0,0 +1,24 @@
+//THE FIRST TEAM WITH ALL GIRLS POSSIBLY EVER (The Powerpuff Girls) -- Nadine Jackson the Bold, T Fabiha, Soojin Choi
+//APCS1 pd2
+//HW31 -- Ye Olde Role Playing Game, Expanded
+//2017-11-13   
+
+public class PlagueDoctor extends Protaganist{
+    
+    public PlagueDoctor(){
+	super();
+	HP += 25;
+    }
+
+    public PlagueDoctor(String newName){
+	this();
+	name = newName;
+    }
+    
+    public String about(){
+	return "In his/her fashionable beak mask, totally legit medical degree in hand, the Plague Doctor is ready to battle both disease and monsters. To be a Plague Doctor, one must have a strong sense of...humor. Armed with leeches and the unearned trust of the public, the Plague Doctor has such maxims to live by as: \"Don't hang around corpses, you idiot\" and \"Ew, don't drink that water. It's gross.\""; 
+    }
+
+    public static void main(String[] args){
+    }
+}
