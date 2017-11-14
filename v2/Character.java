38a3f807dbbfb6df8aa8ef69a92e1b8a7ce787c5
tfabiha <tfabiha@stuy.edu>
2017-11-13 21:02:08 -0500

    Finalized v2

---
+++ v2/Character.java
@@ -0,0 +1,75 @@
+public class Character{
+    //////////////
+    //Attributes//
+    //////////////
+    private String name;
+    private int HP;
+    private int strength;
+    private int defense;
+    private double attackRate;
+
+    public Character() {
+	HP = 150;
+	strength = (int) (Math.random() * 45) + 20;
+	defense = 20;
+	attackRate = 1;
+	name = "Monster";
+    }
+
+    public Character(String newName, int newHP, int newStrength, int newDefense, double newAttackRate) {
+	this();
+	name = newName;
+	HP = newHP;
+        strength = newStrength;
+	defense = newDefense;
+	attackRate = newAttackRate;
+    }
+
+
+    //////////////
+    //Methods//
+    ///////////
+    public boolean isAlive() {
+	return HP > 0;
+    }
+
+    public int getDefense() {
+	return defense;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public double getAttackRate() {
+	return attackRate;
+    }
+
+    public void lowerHP(int lostHP) {
+	if (HP < lostHP){
+	    HP = 0;
+	}
+	else{
+	    HP -= lostHP;
+	}
+    }
+
+    public void setDefense(int newDefense) {
+	defense = newDefense;
+    }
+
+    public void setAttackRate(double newAttackRate) {
+	attackRate = newAttackRate;
+    }
+    
+    public int attack(Character propro) {
+	int damage = (int)(strength * attackRate) - propro.getDefense();
+	propro.lowerHP(damage);
+	return damage;
+    }
+
+    //main method
+    public static void main (String [] args){
+	
+    }
+}
