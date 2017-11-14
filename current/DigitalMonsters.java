c646f374d3c4f23a130928b7b27163c9d2cee94d
Soojin Choi <soojinchoi@Soojins-MacBook-Air.local>
2017-11-13 19:43:32 -0500

    Added DigitalMonsters class

---
+++ current/DigitalMonsters.java
@@ -0,0 +1,25 @@
+//THE FIRST TEAM WITH ALL GIRLS POSSIBLY EVER (The Powerpuff Girls) -- Nadine Jackson the Bold, T Fabiha, Soojin Choi
+//APCS1 pd2
+//HW31 -- Ye Olde Role Playing Game, Expanded 
+//2017-11-13
+
+public class DigitalMonsters extend Monster{
+
+    public DigitalMonsters(){
+	super();
+	strength += 25;
+    }
+
+    public DigitalMonsters(String newName){
+	this();
+	name = newName;
+    }
+
+    public static String about (){
+	return "";
+    }
+
+    public static void main(String [] args){
+
+    }
+}
